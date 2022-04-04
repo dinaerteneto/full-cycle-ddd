e3b71b5f2a836993c339ac0010cc96385566b3c0
Dinaerte Neto <dinaerteneto@gmail.com>
2022-04-04 09:09:44 -0300

    test: should register an event handler add test

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -9,6 +9,7 @@ describe("Domain events tests", () => {
         eventDispatcher.register("ProductCreatedEvent", eventHandler)
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined()
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1)
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler)
     });
 
 })
